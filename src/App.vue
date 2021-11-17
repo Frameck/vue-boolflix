dbb3d3682f8380fc2b2972a30fe43bde6b5e6942
Frameck <fra9879@gmail.com>
2021-11-17 17:57:18 +0100

    Completed milestone 2

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
 	<div id="app">
 		<div class="input-group">
 			<div class="form-outline">
-				<input type="search" id="searchForm" class="form-control" v-model="searchedQuery" @keyup.enter="apiCall('movie', searchedQuery)"/>
+				<input type="search" id="searchForm" class="form-control" v-model="searchedQuery" @keyup.enter="apiCall(['movie', 'tv'], searchedQuery)"/>
 				<!-- <label class="form-label" for="searchForm">Search</label> -->
-				<button type="button" class="btn btn-primary" @click="apiCall('movie', searchedQuery)">
+				<button type="button" class="btn btn-primary" @click="apiCall(['movie', 'tv'], searchedQuery)">
 					<span>cerca</span>
 					<!-- <i class="fas fa-search"></i> -->
 				</button>
 			</div>
 		</div>
 		<ul>
-			<li v-for="item,i in apiData.movie" :key="i">
-				{{ `Titolo: ${item.title}, Titolo originale: ${item.original_title}, Lingua originale: ${item.original_language}, Voto: ${item.vote_average}` }}
+			<li v-for="movie,i in apiData.movie" :key="`movie_${i}`">
+				{{ `Titolo: ${movie[returnMovieOrSeriesKey('movie', 'title')]}, Titolo originale: ${movie[returnMovieOrSeriesKey('movie', 'original_title')]}, Lingua originale: ${movie[returnMovieOrSeriesKey('movie', 'original_language')]}, Voto: ${movie[returnMovieOrSeriesKey('movie', 'vote_average')]}` }}
+				<img :src="createFlagUrl(movie.flag)" :alt="`flag_${movie.flag}`">
+			</li>
+			<li v-for="tvShow,j in apiData.tv" :key="`tvShow_${j}`">
+				{{ `Titolo: ${tvShow[returnMovieOrSeriesKey('tv', 'title')]}, Titolo originale: ${tvShow[returnMovieOrSeriesKey('tv', 'original_title')]}, Lingua originale: ${tvShow[returnMovieOrSeriesKey('tv', 'original_language')]}, Voto: ${tvShow[returnMovieOrSeriesKey('tv', 'vote_average')]}` }}
+				<img :src="createFlagUrl(tvShow.flag)" :alt="`flag_${tvShow.flag}`">
 			</li>
 		</ul>
 	</div>
@@ -38,20 +43,79 @@ export default {
 				movie: [],
 				tv: []
 			},
+			languageCodes: ['it', 'pt', 'es', 'en', 'ja', 'de', 'fr', 'sv', 'hr', 'ar'],
+			countryCodes: ['it', 'pt', 'es', 'us', 'jp', 'de', 'fr', 'sv', 'hr', 'ar'],
 			searchedQuery: ''
 		}
 	},
 	methods: {
-		apiCall(endpoint, query) {
-			axios.get(this.apiBaseUrl + this.apiEndpoints[endpoint], {
-				params: {
-					api_key: this.apiKey,
-					query
-				}
-			}).then(res => {
-				this.apiData[endpoint] = res.data.results
+		apiCall(endpoints, query) {
+			endpoints.forEach(endpoint => {
+				axios.get(this.apiBaseUrl + this.apiEndpoints[endpoint], {
+					params: {
+						api_key: this.apiKey,
+						query
+					}
+				}).then(res => {
+					this.apiData[endpoint] = [] // svuoto l'array ad ogni chiamata
+	
+					res.data.results.forEach(movie => {
+						const movieLanguage = movie.original_language
+						console.log(movieLanguage)
+						let countryCode = movieLanguage
+	
+						if (this.languageCodes.includes(movieLanguage)) {
+							switch (movieLanguage) {
+								case 'en':
+									countryCode = 'us'
+									break
+								case 'ja':
+									countryCode = 'jp'
+									break
+							}
+							// if (movieLanguage === 'en') {
+							// 	countryCode = 'us'
+							// } else {
+							// 	countryCode = movieLanguage
+							// }
+							
+							movie['flag'] = countryCode
+						}
+	
+						this.apiData[endpoint].push(movie)
+					})
+				})
 			})
+		},
+		createFlagUrl(countryCode) {
+			let flagUrl = ''
+
+			if (this.countryCodes.includes(countryCode)) {
+				flagUrl = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode.toUpperCase()}.svg`
+			}
+
+
+			return flagUrl
+		},
+		returnMovieOrSeriesKey(type, key) {
+			if (type === 'movie') {
+				if (key === 'title') return 'title'
+				if (key === 'original_title') return 'original_title'
+				if (key === 'original_language') return 'original_language'
+				if (key === 'vote_average') return 'vote_average'
+			} else {
+				if (key === 'title') return 'name'
+				if (key === 'original_title') return 'original_name'
+				if (key === 'original_language') return 'original_language'
+				if (key === 'vote_average') return 'vote_average'
+			}
 		}
+	},
+	computed: {
+		// returnMovieOrSeriesArray() {
+		// 	if (this.apiData.movie.length !== 0) return this.apiData.movie
+		// 	return this.apiData.tv
+		// }
 	},
 	mounted() {
 		// this.apiCall('movie', 'ciao')
@@ -60,13 +124,16 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	img {
+		width: 25px;
+	}
 // @import'~bootstrap/dist/css/bootstrap.css';
 </style>
